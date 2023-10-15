2ff561c4c82fb341f24bf14572e71f4c50a0ef73
NjukiG <91128064+NjukiG@users.noreply.github.com>
2023-10-15 14:57:57 +0300

    Time: 79 ms, Memory: 48 MB - LeetHub

---
+++ valid-anagram/valid-anagram.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    // console.log(helper(s))
+    // console.log(helper(t))
+    // if(helper(s) === helper(t)){
+    //     return true
+    // }else{return false}
+
+    return helper(s) === helper(t)
+
+};
+
+var helper = function (str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+}
