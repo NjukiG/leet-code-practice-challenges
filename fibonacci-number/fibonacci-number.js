0fa0a079cb188a5f07197f2129f3b3086db8fe45
NjukiG <91128064+NjukiG@users.noreply.github.com>
2023-10-18 17:16:10 +0300

    Time: 44 ms, Memory: 41.7 MB - LeetHub

--- fibonacci-number/fibonacci-number.js
+++ fibonacci-number/fibonacci-number.js
@@ -2,10 +2,12 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
+var fib = function(n, memo={}) {
+    if(n in memo) return memo[n]
     if(n === 0) return 0
     if(n === 1) return 1
     
-    return fib(n - 1) + fib(n- 2)
+    memo[n] = fib(n - 1, memo) + fib(n- 2, memo)
+    return memo[n]
     
 };
