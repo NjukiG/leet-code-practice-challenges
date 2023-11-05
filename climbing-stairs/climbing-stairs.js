087eed184ccda41a854ecd5607dd08fdba52c452
NjukiG <91128064+NjukiG@users.noreply.github.com>
2023-11-05 16:40:05 +0300

    Time: 62 ms, Memory: 41.5 MB - LeetHub

--- climbing-stairs/climbing-stairs.js
+++ climbing-stairs/climbing-stairs.js
@@ -3,12 +3,14 @@
  * @return {number}
  */
 var climbStairs = function (n) {
+    // console.log(n)
 // It's almost like fibonacci
     let ways = [1, 2]
 
     for (let i = 2; i <= n; i++) {
         ways[i] = ways[i - 1] + ways[i - 2]
     }
+    // console.log(ways)
     return ways[n - 1]
 
 };
