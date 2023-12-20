2551d93ae21b450d9111a791bf2319265e9c15ff
NjukiG <91128064+NjukiG@users.noreply.github.com>
2023-12-20 14:44:22 +0300

    Time: 69 ms, Memory: 54.9 MB - LeetHub

--- contains-duplicate/contains-duplicate.js
+++ contains-duplicate/contains-duplicate.js
@@ -4,15 +4,23 @@
  */
 var containsDuplicate = function(nums) {
     if(nums.length <=1) return false
-    let array = new Array()
-    for(let num of nums){
-        if(array.includes(num)){
-            return true
-        }
-        array.push(num)
-        // console.log(array)
-    }
+    // let array = new Array()
+    // for(let num of nums){
+    //     if(array.includes(num)){
+    //         return true
+    //     }
+    //     array.push(num)
+    //     // console.log(array)
+    // }
     
+    let set = new Set();
+    
+    for(let num of nums){
+        if(set.has(num)){
+            return true;
+        }
+        set.add(num);
+    }    
     return false
     
 };
