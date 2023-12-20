e71394b3990b6dd50dfa9cc1b1fba89b5acb7f91
NjukiG <91128064+NjukiG@users.noreply.github.com>
2023-12-20 14:25:11 +0300

    Time: 50 ms, Memory: 41.8 MB - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
-    let maxProfit = 0
+    let profit = 0;
     
     for(let i = 1; i < prices.length; i++){
-        if(prices[i] > prices[i - 1]){
-            maxProfit = maxProfit + (prices[i] - prices[i - 1])
+        if(prices[i] > prices[i -1]){
+            profit = profit + (prices[i] - prices[i - 1]);
         }
     }
-    return maxProfit
+    return profit
     
 };
