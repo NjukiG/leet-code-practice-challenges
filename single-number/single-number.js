3fb8c7a8aa8167e3f777e639935c541abf97403b
NjukiG <91128064+NjukiG@users.noreply.github.com>
2024-02-14 17:55:35 +0300

    Time: 56 ms, Memory: 56.9 MB - LeetHub

--- single-number/single-number.js
+++ single-number/single-number.js
@@ -4,19 +4,31 @@
  */
 var singleNumber = function(nums) {
     // console.log(nums)
-   let obj = {}
+//    let obj = {}
    
-   for(let num of nums){
-       if(obj[num]){
-           obj[num]++
-       }else{
-           obj[num] = 1
-   }
-   }
+//    for(let num of nums){
+//        if(obj[num]){
+//            obj[num]++
+//        }else{
+//            obj[num] = 1
+//    }
+//    }
     
-    for(let item in obj){
-        if(obj[item] === 1) return item
+//     for(let item in obj){
+//         if(obj[item] === 1) return item
+//     }
+    
+    let itemMap = new Map()
+    
+    for(let num of nums){
+        itemMap.set(num, (itemMap.get(num) || 0) + 1)
     }
+    
+    for(let [key, count] of itemMap){
+        if(count === 1) return key
+    }
+    
+    return null
     
    
     
